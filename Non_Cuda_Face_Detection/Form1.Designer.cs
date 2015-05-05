104621e44749c12446bd074be2d20cb8db9d7c0d
zubinengg <zubin.engg@gmail.com>
2015-05-05 21:42:49 +0530

    First Commit

---
+++ Non_Cuda_Face_Detection/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Non_Cuda_Face_Detection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageBox1.Location = new System.Drawing.Point(13, 13);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(515, 498);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            this.imageBox1.Click += new System.EventHandler(this.imageBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(535, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageBox2.Location = new System.Drawing.Point(616, 12);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(173, 166);
+            this.imageBox2.TabIndex = 4;
+            this.imageBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1069, 523);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+    }
+}
+
---
+++ Non_Cuda_Face_Detection/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Emgu.CV.CvEnum;
+
+
+namespace Non_Cuda_Face_Detection
+{
+    public partial class Form1 : Form
+    {
+        private HaarCascade haar;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            haar = new HaarCascade("haarcascade_frontalface_default.xml");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imageBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void set_image(Image<Bgr, Byte> source, Rectangle crop)
+        {
+            try
+            {
+                imageBox2.Image = source;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fileName = "W:\\research\\1.tif";
+            imageBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            imageBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            Image<Bgr, Byte> ImageFrame = new Image<Bgr, Byte>(@"W:\research\1.jpg");
+            imageBox1.Image = ImageFrame;
+            if (ImageFrame != null)
+            {
+                Image<Gray, byte> grayframe = ImageFrame.Convert<Gray, byte>();
+                var faces = grayframe.DetectHaarCascade(haar, 1.4, 4,
+                    HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+                    new Size(25, 25))[0];
+                //var temp = grayframe.DetectHaarCascade(haar, 1.4 ,6, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE,)
+                //testing
+                foreach (var face in faces)
+                {
+                    ImageFrame.Draw(face.rect, new Bgr(Color.Green), 3);
+                    set_image(ImageFrame, face.rect);
+                }
+
+            }
+
+        }
+    }
+}
