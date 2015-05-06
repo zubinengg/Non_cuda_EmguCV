cf78f0c1a6f8596cdf2b1adbf913ac4ae62b601b
zubinengg <zubin.engg@gmail.com>
2015-05-06 12:18:22 +0530

    dll added

--- Non_Cuda_Face_Detection/Form1.cs
+++ Non_Cuda_Face_Detection/Form1.cs
@@ -19,12 +19,14 @@ namespace Non_Cuda_Face_Detection
     public partial class Form1 : Form
     {
         private HaarCascade haar;
+        private CascadeClassifier haar1;
 
 
         public Form1()
         {
             InitializeComponent();
             haar = new HaarCascade("haarcascade_frontalface_default.xml");
+            //haar1 = new CascadeClassifier("haarcascade_frontalface_default.xml");
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -42,6 +44,7 @@ namespace Non_Cuda_Face_Detection
         {
             try
             {
+                source.ROI = crop;
                 imageBox2.Image = source;
             }
             catch
@@ -61,14 +64,18 @@ namespace Non_Cuda_Face_Detection
             if (ImageFrame != null)
             {
                 Image<Gray, byte> grayframe = ImageFrame.Convert<Gray, byte>();
-                var faces = grayframe.DetectHaarCascade(haar, 1.4, 6,
-                    HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
-                    new Size(25, 25))[0];
-                //var temp = grayframe.DetectHaarCascade(haar, 1.4 ,6, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE,)
+                var faces = grayframe.DetectHaarCascade(haar, 1.4, 6,HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,new Size(25, 25))[0];
+                //var faces = greyframe.CascadeClassifier(haar1, 1.4, 6, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(25, 25))[0];
+                int count = 0;
                 foreach (var face in faces)
                 {
+                    count += 1;
                     ImageFrame.Draw(face.rect, new Bgr(Color.Green), 3);
                     set_image(ImageFrame, face.rect);
+                    if (count>1)
+                    {
+                        break;
+                    }
                 }
 
             }             
