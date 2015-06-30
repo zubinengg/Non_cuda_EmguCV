8bf462d7292d58065e7e6953766a7adaeb481a93
zubinengg <zubin.engg@gmail.com>
2015-06-30 20:30:51 +0530

    LBP verified

--- Non_Cuda_Face_Detection/Form1.Designer.cs
+++ Non_Cuda_Face_Detection/Form1.Designer.cs
@@ -42,23 +42,29 @@
             this.label5 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.imageBox3 = new Emgu.CV.UI.ImageBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // imageBox1
             // 
             this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.imageBox1.Location = new System.Drawing.Point(13, 39);
+            this.imageBox1.Location = new System.Drawing.Point(13, 42);
             this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(515, 472);
+            this.imageBox1.Size = new System.Drawing.Size(296, 338);
             this.imageBox1.TabIndex = 2;
             this.imageBox1.TabStop = false;
             this.imageBox1.Click += new System.EventHandler(this.imageBox1_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(535, 13);
+            this.button1.Location = new System.Drawing.Point(497, 14);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
@@ -69,7 +75,7 @@
             // imageBox2
             // 
             this.imageBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.imageBox2.Location = new System.Drawing.Point(534, 345);
+            this.imageBox2.Location = new System.Drawing.Point(315, 42);
             this.imageBox2.Name = "imageBox2";
             this.imageBox2.Size = new System.Drawing.Size(173, 166);
             this.imageBox2.TabIndex = 4;
@@ -79,7 +85,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(534, 39);
+            this.label1.Location = new System.Drawing.Point(496, 39);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(161, 13);
             this.label1.TabIndex = 5;
@@ -89,7 +95,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(534, 65);
+            this.label2.Location = new System.Drawing.Point(496, 52);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(123, 13);
             this.label2.TabIndex = 6;
@@ -99,7 +105,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(534, 92);
+            this.label3.Location = new System.Drawing.Point(497, 81);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(95, 13);
             this.label3.TabIndex = 7;
@@ -109,7 +115,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(534, 121);
+            this.label4.Location = new System.Drawing.Point(497, 110);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(122, 13);
             this.label4.TabIndex = 8;
@@ -117,21 +123,22 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(663, 63);
+            this.textBox1.Location = new System.Drawing.Point(626, 52);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 9;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(663, 89);
+            this.textBox2.Location = new System.Drawing.Point(626, 78);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 10;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(663, 115);
+            this.textBox3.Location = new System.Drawing.Point(626, 104);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 11;
@@ -140,7 +147,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(534, 164);
+            this.label5.Location = new System.Drawing.Point(497, 153);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(175, 13);
             this.label5.TabIndex = 12;
@@ -150,7 +157,7 @@
             // 
             this.textBox4.Location = new System.Drawing.Point(89, 16);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(439, 20);
+            this.textBox4.Size = new System.Drawing.Size(339, 20);
             this.textBox4.TabIndex = 13;
             // 
             // label6
@@ -163,11 +170,62 @@
             this.label6.TabIndex = 14;
             this.label6.Text = "Image Path";
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(497, 182);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(174, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Number of Eyes Detected = 0";
+            // 
+            // imageBox3
+            // 
+            this.imageBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageBox3.Location = new System.Drawing.Point(616, 198);
+            this.imageBox3.Name = "imageBox3";
+            this.imageBox3.Size = new System.Drawing.Size(333, 323);
+            this.imageBox3.TabIndex = 16;
+            this.imageBox3.TabStop = false;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(0, 527);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(713, 92);
+            this.textBox5.TabIndex = 17;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(500, 224);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 18;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(498, 208);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Radius";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(776, 516);
+            this.ClientSize = new System.Drawing.Size(961, 631);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.imageBox3);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label5);
@@ -186,6 +244,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -206,6 +265,11 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private Emgu.CV.UI.ImageBox imageBox3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- Non_Cuda_Face_Detection/Form1.cs
+++ Non_Cuda_Face_Detection/Form1.cs
@@ -19,7 +19,9 @@ namespace Non_Cuda_Face_Detection
     public partial class Form1 : Form
     {
         private HaarCascade haar;
-        private CascadeClassifier haar1;
+        //private CascadeClassifier eye;
+        private HaarCascade eye;
+        //private CascadeClassifier haar1;
 
 
         public Form1()
@@ -29,8 +31,11 @@ namespace Non_Cuda_Face_Detection
             this.textBox1.Text = "1.2";
             this.textBox2.Text = "4";
             this.textBox3.Text = "25";
-            this.textBox4.Text = @"W:\research\2.jpg";
+            this.textBox4.Text = @"W:\research\1.tif";
+            this.textBox6.Text = "1";
             //haar1 = new CascadeClassifier("haarcascade_frontalface_default.xml");
+            //eye = new CascadeClassifier("haarcascade_eye_tree_eyeglasses.xml");
+            eye = new HaarCascade("haarcascade_eye.xml");
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -42,14 +47,117 @@ namespace Non_Cuda_Face_Detection
         {
 
         }
+        Bitmap NormalizeLbpMatrix(double[,] Mat, Bitmap lbp, double max)
+        {
+            int NumRow = lbp.Height;
+            int numCol = lbp.Width;
+            for (int i = 0; i < NumRow; i++)
+            {
+                for (int j = 0; j < numCol; j++)
+                {
+                    // see the Normalization process of dividing pixel by max value and multiplying with 255
+                    double d = Mat[j, i] / max;
+                    int v = (int)(d * 255);
+                    Color c = Color.FromArgb(v, v, v);
+                    lbp.SetPixel(j, i, c);
+                }
+            }
+            return lbp;
+        }
+        double Bin2Dec(List<int> vals)
+        {
+            double mult = 256;
+            double sum = 0;
+            //Boolean test = false;
+            foreach (int v in vals)
+            {
+                sum += mult * v;
+                mult /= 2;
+            }
+            return sum;
+        }
+        Bitmap LBP(Bitmap srcBmp, int R)
+        {
+            // We want to get LBP image from srcBmp and window R
+            Bitmap bmp = srcBmp;
+            //1. Extract rows and columns from srcImage . Note Source image is Gray scale Converted Image
+            int NumRow = srcBmp.Height;
+            int numCol = srcBmp.Width;
+            Bitmap lbp = new Bitmap(numCol, NumRow);
+            Bitmap GRAY = new Bitmap(numCol, NumRow);// GRAY is the resultant matrix 
+            double[,] MAT = new double[numCol, NumRow];
+            double max = 0.0;
+            //2. Loop through Pixels
+            for (int i = 0; i < NumRow; i++)
+            {
+                for (int j = 0; j < numCol; j++)
+                {
+                    //  Color c1=Color.FromArgb(0,0,0);
+                    MAT[j, i] = 0;
+                    //lbp.SetPixel(j, i,c1) ;
+
+
+                    //define boundary condition, other wise say if you are looking at pixel (0,0), it does not have any suitable neighbors
+                    if ((i > R) && (j > R) && (i < (NumRow - R)) && (j < (numCol - R)))
+                    {
+                        // we want to store binary values in a List
+                        List<int> vals = new List<int>();
+                        try
+                        {
+                            for (int i1 = i - R; i1 < (i + R); i1++)
+                            {
+                                for (int j1 = j - R; j1 < (j + R); j1++)
+                                {
+                                    int acPixel = srcBmp.GetPixel(j, i).R;
+                                    int nbrPixel = srcBmp.GetPixel(j1, i1).R;
+                                    // 3. This is the main Logic of LBP
+                                    if (nbrPixel > acPixel)
+                                    {
+                                        vals.Add(1);
+                                    }
+                                    else
+                                    {
+                                        vals.Add(0);
+                                    }
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                        }
+                        //4. Once we have a list of 1's and 0's , convert the list to decimal
+                        // Also for normalization purpose calculate Max value
+                        double d1 = Bin2Dec(vals);
+                        MAT[j, i] = d1;
+                        if (d1 > max)
+                        {
+                            max = d1;
+                        }
+                    }
+
+                    //////////////////
+
+
+                }
+            }
+            //5. Normalize LBP matrix MAT an obtain LBP image lbp
+            lbp = NormalizeLbpMatrix(MAT, lbp, max);
+            return lbp;
+        }
+
+
 
 
         private void set_image(Image<Bgr, Byte> source, Rectangle crop)
         {
             try
             {
-                source.ROI = crop;
-                imageBox2.Image = source;
+                Image<Bgr, Byte> ImageFrame = new Image<Bgr, Byte>(this.textBox4.Text.ToString());
+                //Image<Bgr, Byte> region=source;
+                ImageFrame.ROI = crop;
+                imageBox2.Image = ImageFrame.Convert<Gray, byte>();
+                imageBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+                imageBox3.Image = new Image<Bgr, Byte>(LBP(ImageFrame.ToBitmap(), Convert.ToInt16(this.textBox6.Text.ToString())));
                 // TESTING FOR COMMIT
             }
             catch
@@ -58,9 +166,9 @@ namespace Non_Cuda_Face_Detection
             }
         }
 
-        private Rectangle get_big_rect(Rectangle A,Rectangle B)
+        private Rectangle get_big_rect(Rectangle A, Rectangle B)
         {
-            if (A.Width*A.Height>B.Width*B.Height)
+            if (A.Width * A.Height > B.Width * B.Height)
             {
                 return A;
             }
@@ -68,12 +176,11 @@ namespace Non_Cuda_Face_Detection
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            string fileName = "W:\\research\\1.tif";
             imageBox1.SizeMode = PictureBoxSizeMode.Zoom;
             imageBox2.SizeMode = PictureBoxSizeMode.Zoom;
             Image<Bgr, Byte> ImageFrame = new Image<Bgr, Byte>(this.textBox4.Text.ToString());
             imageBox1.Image = ImageFrame;
-            
+
             if (ImageFrame != null)
             {
                 double scaleIncreaseRate = Convert.ToDouble(this.textBox1.Text.ToString());
@@ -85,9 +192,17 @@ namespace Non_Cuda_Face_Detection
                     scaleIncreaseRate,
                     minNeighbours, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
                     new Size(minDetectionScale, minDetectionScale))[0];
+
+                //Eyes 
+                //var eyes = eye.DetectMultiScale(grayframe, 1.2, 1, new Size(25, 25), new Size(500, 500));
+                var eyes = grayframe.DetectHaarCascade(eye, 1.1, 2, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(25, 25));
+
+
+
+
                 //var faces = greyframe.CascadeClassifier(haar1, 1.4, 6, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(25, 25))[0];
-                
-                Rectangle bigFace= new Rectangle(0,0,0,0);
+
+                Rectangle bigFace = new Rectangle(0, 0, 0, 0);
                 if (faces.Length > 0)
                 {
                     foreach (var face in faces)
@@ -95,10 +210,41 @@ namespace Non_Cuda_Face_Detection
                         ImageFrame.Draw(face.rect, new Bgr(Color.Green), 3);
                         bigFace = get_big_rect(bigFace, face.rect);
                     }
-                    set_image(ImageFrame, bigFace);
+
                     this.label5.Text = "Number of Face detected = " + faces.Length.ToString();
+                    set_image(ImageFrame, bigFace);
+                    //ImageFrame.ROI = bigFace;
                 }
-            }             
+
+
+                if (eyes.Length > 0)
+                {
+                    int loop = 0;
+                    foreach (var eyesnap in eyes[0])
+                    {
+                        //Rectangle eyeRect = eyesnap.rect;
+                        ImageFrame.Draw(eyesnap.rect, new Bgr(Color.Blue), 2);
+                        loop += 1;
+                    }
+                    this.label7.Text = "Number of Eyes detected = " + (eyes.Length * 2).ToString();
+                    //this.label7.Text = "Number of Eyes detected = " + (loop).ToString();
+                }
+
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
